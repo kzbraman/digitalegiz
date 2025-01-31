d6ef9b175cec9f74dec5f37ba290d94ce17b3e5a
TimurIshmurzinDS <timon.ishmurzin@gmail.com>
2025-01-31 19:43:05 +0500

    completed

--- src/main/java/org/digitalegiz/api/resource/UserResource.java
+++ src/main/java/org/digitalegiz/api/resource/UserResource.java
@@ -103,9 +103,9 @@ public class UserResource extends BaseObjectResource<User> {
             } else {
                 if (UserUtil.isEmpty(storage)) {
                     entity.setAdministrator(true);
-                } else if (!permissionsService.getServer().getRegistration()) {
-                    throw new SecurityException("Registration disabled");
-                }
+                } //else if (!permissionsService.getServer().getRegistration()) {
+                    //throw new SecurityException("Registration disabled");
+                //}
                 if (permissionsService.getServer().getBoolean(Keys.WEB_TOTP_FORCE.getKey())
                         && entity.getTotpKey() == null) {
                     throw new SecurityException("One-time password key is required");
